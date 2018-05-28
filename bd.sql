2df761e385ee8474b0d8037942ef97c2cee5c217
Luis Avila <avilaluis0@hotmail.com>
2018-05-28 00:29:12 -0500

    Agregada tabla categorias a la base de datos

--- bd.sql
+++ bd.sql
@@ -1,29 +1,35 @@
--- MySQL dump 10.13  Distrib 5.7.17, for macos10.12 (x86_64)
+-- phpMyAdmin SQL Dump
+-- version 4.7.9
+-- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1    Database: udc_blog
--- ------------------------------------------------------
--- Server version	5.5.5-10.1.25-MariaDB
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 28-05-2018 a las 07:26:55
+-- Versión del servidor: 10.1.31-MariaDB
+-- Versión de PHP: 5.6.34
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
--- Table structure for table `blog`
+-- Base de datos: `udc_blog`
 --
 
-DROP TABLE IF EXISTS `blog`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `blog`
+--
+
 CREATE TABLE `blog` (
-  `id` int(10) unsigned zerofill NOT NULL,
+  `id` int(10) UNSIGNED ZEROFILL NOT NULL,
   `codigo` varchar(45) DEFAULT NULL,
   `titulo` varchar(100) DEFAULT NULL,
   `intro` text,
@@ -31,28 +37,45 @@ CREATE TABLE `blog` (
   `publicar` char(1) DEFAULT NULL,
   `estado` varchar(45) DEFAULT NULL,
   `idUsuario` int(10) DEFAULT NULL,
-  `idCategoria` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  `idCategoria` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `blog`
+-- Volcado de datos para la tabla `blog`
 --
 
-LOCK TABLES `blog` WRITE;
-/*!40000 ALTER TABLE `blog` DISABLE KEYS */;
-INSERT INTO `blog` VALUES (0000000000,'B0001','MI PRIMER BLOG','este es mi primer blog espere que les guste','2018-05-26 04:06:06','S','R',1,1);
-/*!40000 ALTER TABLE `blog` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+INSERT INTO `blog` (`id`, `codigo`, `titulo`, `intro`, `fechaPublicacion`, `publicar`, `estado`, `idUsuario`, `idCategoria`) VALUES
+(0000000000, 'B0001', 'MI PRIMER BLOG', 'este es mi primer blog espere que les guste', '2018-05-26 04:06:06', 'S', 'R', 1, 1);
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `categorias`
+--
+
+CREATE TABLE `categorias` (
+  `id` int(10) UNSIGNED ZEROFILL NOT NULL,
+  `nombre` varchar(20) DEFAULT NULL,
+  `descripcion` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `blog`
+--
+ALTER TABLE `blog`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `categorias`
+--
+ALTER TABLE `categorias`
+  ADD PRIMARY KEY (`id`);
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2018-05-26 13:13:28
